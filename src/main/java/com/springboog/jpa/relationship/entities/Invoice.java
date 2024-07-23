21a80f671ab62964df504e0475baf4728c562ee7
LuisMunar <luis.emunarr@gmail.com>
2024-07-23 14:16:15 -0500

    Video 154 ready

--- src/main/java/com/springboog/jpa/relationship/entities/Invoice.java
+++ src/main/java/com/springboog/jpa/relationship/entities/Invoice.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,6 +16,9 @@ public class Invoice {
 
   private String description;
   private Long total;
+
+  @ManyToOne
+  private Client client;
 
   public Invoice() {}
 
@@ -45,5 +49,10 @@ public class Invoice {
 
   public void setTotal(Long total) {
     this.total = total;
+  }
+
+  @Override
+  public String toString() {
+    return "{id=" + id + ", description=" + description + ", total=" + total + ", client=" + client + "}";
   }
 }
