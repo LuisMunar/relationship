2d9e125cc73542083c7da3058f27105e6fa15361
LuisMunar <luis.emunarr@gmail.com>
2024-07-23 08:34:41 -0500

    Video 153 ready

---
+++ src/test/java/com/springboog/jpa/relationship/RelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboog.jpa.relationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
